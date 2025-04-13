3af377c761d00270853b341c35edb35895adbee0
bycrafter <gungor.ae@gmail.com>
2025-04-13 19:10:14 +0300

    Refactor method names for clarity in BaseRepository
    Renamed `update` to `updateEntity` and `save` to `saveEntity` to improve method clarity and maintain consistency in naming conventions. These changes enhance code readability and make the methods' purposes more explicit.

--- src/main/java/datajpa/repository/BaseRepository.java
+++ src/main/java/datajpa/repository/BaseRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 
 public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, String> {
-    default T update(T entity){
+    default T updateEntity(T entity){
         T oldEntity = findById(entity.getId()).orElseThrow(() -> new RuntimeException("No updatable user"));
         entity.setCreatedAt(oldEntity.getCreatedAt());
         entity = saveAndFlush(entity);
         return entity;
     }
-    default T save(T entity){
+    default T saveEntity(T entity){
         LocalDateTime now = LocalDateTime.now();
         entity.setCreatedAt(now);
         entity.setUpdatedAt(now);
